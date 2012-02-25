e44b6003fb69dd4467ed4f5bb073e7e8af8673d9
Joachim Kudish <joachim.kudish@gmail.com>
2012-02-25 12:18:02 -0800

    local config sample

---
+++ web/application/config/local-config-sample.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Set Local Config/Env variables
+ */
+
+putenv('sitetitle=In My Backyard');
+putenv('url=localdev/inmybackyard/web');
+putenv('mysqlhost=localhost');
+putenv('mysqldb=inmb');
+putenv('mysqluser=root');
+putenv('mysqlpw=root');
