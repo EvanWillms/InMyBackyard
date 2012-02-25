c1e81bed7504ed3bace01c9868e0594cec34ecdc
badjer <brian.dejong@gmail.com>
2012-02-25 11:58:43 -0800

    KML library added

--- loader/imbloader/project.clj
+++ loader/imbloader/project.clj
@@ -1,3 +1,4 @@
 (defproject imbloader "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [de.micromata.jak/JavaAPIforKml "2.2.0"]])
