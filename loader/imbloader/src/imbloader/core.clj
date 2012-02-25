1e1ace61a5f2d12ce6e0e37276b64bb4ee2cb4bc
badjer <brian.dejong@gmail.com>
2012-02-25 14:05:09 -0800

    First draft of loader

--- loader/imbloader/src/imbloader/core.clj
+++ loader/imbloader/src/imbloader/core.clj
@@ -1,1 +1,47 @@
-(ns imbloader.core)
+(ns imbloader.core
+  (require [clojure.xml :as xml]
+           [clojure.contrib.sql :as sql]
+           [clojure.string :as s]))
+
+(def testloc "/Users/brian/Downloads/kml_road_ahead/road_closures.kml")
+
+(defn getfile [uri]
+  "Load the kml file from the supplied uri"
+  (xml/parse uri))
+
+(defn getnodes [xml node]
+  (for [x (xml-seq xml) :when (= node (:tag x))] x))
+
+(defn gettext [xml node]
+  (let [n (getnodes xml node)]
+    (if (empty? n)
+      nil
+      (first (:content (first n))))))
+
+(defn placemarks [file]
+  (getnodes file :Placemark))
+
+(defn tomap [p]
+  "Convert a placemark xml node to a map we can use"
+  (let [nam (gettext p :name)
+        desc (gettext p :description)
+        coords (s/split (gettext p :coordinates) #"\,")
+        lat (first coords)
+        lon (second coords)
+        ]
+    {:name nam :description desc :latitude lat :longitude lon}))
+
+
+(defn write [events]
+  (sql/with-connection* {:classname "oracle.jdbc.driver.OracleDriver"
+                  :subprotocol "oracle"
+                  :subname (str "thin:@//" host ":1521/" db)
+                  :user "joe"
+                  :password "secret"}
+                   (sql/insert-records "Events" events)))
+
+(defn loadfrom [uri]
+  (let [file (getfile uri)
+        placemarks (placemarks file)
+        events (map tomap placemarks)]
+    (write events)))
